5bda9a1020d5b9893a260cf91af4c3df360d0a03
Reg Onglao <mrpsonglao@gmail.com>
2017-09-09 00:20:18 +0800

    revisions to Tourism files based on feedback from JM (WBG Tourism). Key revisions include: - added indicator descriptions to address Open Data unit's feedback to make the report more layman-friendly - added/removed indicators based on your inputs - renamed/removed section titles based on your inputs - changed 2nd sparkline (Int. Tourism Receipts) unit from "billion" to "million" - removed regional comparison for first line chart (Passenger Arrivals) - improved legend spacing for radar chart (made it vertical instead)

--- templates/Tourism_charts.R
+++ templates/Tourism_charts.R
@@ -35,7 +35,7 @@ figure_sparkline(Report_data,reportConfig,couName, "figure6",rankBig=FALSE)
 ########## Tourism Demand and Supply ##########
 
 ## ---- line1.1 ----
-line_chart(Report_data,reportConfig,couName,reportConfig$Section[1],"line1",minTime="2005",neighbor="region",max_neighbors = 1)
+line_chart(Report_data,reportConfig,couName,reportConfig$Section[1],"line1",minTime="2005",neighbor="region",max_neighbors = 0)
 
 ## ---- line1.2 ----
 line_chart(Report_data,reportConfig,couName,reportConfig$Section[1],"line2",minTime="2005",neighbor="region",max_neighbors = 0)
@@ -63,21 +63,21 @@ radar_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],c("radar1
 bar_chart(Report_data,reportConfig,couName, reportConfig$Section[1:4],c("bar1","bar2","bar3"),paste_unit = FALSE, percentBar = TRUE)
 
 ## ---- number2.1 ----
-number_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],"line5",str_wrap_size=30,rankBig=FALSE,includeUnit = FALSE)
+number_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],"line5",str_wrap_size=30,rankBig=FALSE,includeUnit = TRUE)
 
 ## ---- bar2.2 ----
 bar_chart(Report_data,reportConfig,couName, reportConfig$Section[1:4],"line3",paste_unit = FALSE)
 
 ## ---- number4.1 ----
-number_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],"radar3",str_wrap_size=30,rankBig=TRUE)
+number_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],"radar3",str_wrap_size=27,rankBig=TRUE)
 
 ########## Other Competitiveness Indicators
 
 ## ---- number4.2 ----
-number_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],c("radar4","radar8"),str_wrap_size=30,rankBig=TRUE)
+number_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],c("radar4","radar8"),str_wrap_size=27,rankBig=TRUE)
 
 ## ---- number4.3 ----
-number_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],c("radar5"),str_wrap_size=30,rankBig=TRUE)
+number_chart(Report_data,reportConfig,couName,reportConfig$Section[1:4],c("radar5"),str_wrap_size=27,rankBig=TRUE)
 
 
 
