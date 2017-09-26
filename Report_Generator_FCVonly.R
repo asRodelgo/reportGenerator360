6e54ae0a721b145e02662488cf95ed8295e5068c
Reg Onglao <mrpsonglao@gmail.com>
2017-09-26 22:04:09 +0800

    make Report_Generator code for FCV reports (only for 192 countries with FCV 2017 data)

---
+++ Report_Generator_FCVonly.R
@@ -0,0 +1,15 @@
+# Run this script to generate all the country PDF reports for Investment Climate (FCV) only
+# List of countries is based on intersection of TCdata360 country list and Harmonized FCV 2017 list (from WBG IC-FCS team)
+##################################
+# setwd() to handle images and other files
+
+setwd('/Users/mrpso/Documents/GitHub/reportGenerator360/')
+source('global_utils.R') # data and functions needed
+source('helper_functions.R') # charts and table functions needed
+
+# Create the data reports --------------------------------------
+fcv_coulist <- read.csv('templates/FCV_iso3_countrylist.csv', header=FALSE)
+include <- fcv_coulist$V1
+for (couName in filter(countries, (iso3 %in% include))$name) {
+  .reportGenerator(couName, "FCV")
+}
