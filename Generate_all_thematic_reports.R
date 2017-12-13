6a846750d1094974e2348a18e3fbbe478285c547
Reg Onglao <mrpsonglao@gmail.com>
2017-12-13 15:45:19 +0800

    edits to FCV report generation code

--- Generate_all_thematic_reports.R
+++ Generate_all_thematic_reports.R
@@ -7,11 +7,60 @@ for(input_reportID in c("Entrepreneurship", "Tourism")){
 # run Gender
 input_reportID <- "Gender"
 source('global_utils.R')
-source('templates/Gender_charts.R') #run preprocessing code here
+
+# run Gender preprocessing
+Report_data <- ReportDataList[[input_reportID]]
+reportConfig <- ReportConfigList[[input_reportID]]
+dataDesc <- dataDescList[[input_reportID]]
+########## Header ##########
+
+text_color <- "#818181"
+
 source('Report_Generator.R')
 
 # run FCV
 input_reportID <- "FCV"
 source('global_utils.R')
-source('templates/FCV_charts.R') #run preprocessing code here
+source('datapull_TCdata360.R')
+
+#run preprocessing code
+Report_data <- ReportDataList[[input_reportID]]
+reportConfig <- ReportConfigList[[input_reportID]]
+dataDesc <- dataDescList[[input_reportID]]
+
+#### Add new columns to countries dataframe to include FCS typology
+# Add latest resource rich data
+resource_rich <- filter(Report_data, Key %in% 28157) %>%
+  subset(Period == max(Period,na.rm = TRUE)) %>%
+  mutate(ResourceRich = ifelse(Observation %in% 1,"Yes","No"))
+resource_rich <- resource_rich[c('iso2','ResourceRich')]
+countries <- merge(countries, resource_rich, by="iso2", all.x=TRUE)
+
+# Add FCV class
+fcv_coutyp <- filter(Report_data, Key %in% c(28150, 28151, 28152)) %>%
+  subset(Period == max(Period,na.rm = TRUE)) %>%
+  subset(Observation == 1)
+# fcv_coutyp$FCVclass <- sub("^Fragility Class: ", "", fcv_coutyp$IndicatorShort)
+fcv_coutyp$FCVclass <- fcv_coutyp$IndicatorShort
+fcv_coutyp <- fcv_coutyp[c("iso2", "FCVclass")]
+countries <- merge(countries, fcv_coutyp, by="iso2", all.x=TRUE)
+
+# Map longer names to existing country typologies
+countries$incomeLevel_long <- mapvalues(countries$incomeLevel,
+                                        from=c("LIC", "HIC", "UMC", "LMC", "INX"),
+                                        to=c("Low Income", "High Income", "Upper Middle Income",
+                                             "Lower Middle Income", "Upper Middle Income"))
+countries <- mutate(countries, sids_long = ifelse(sids,"Yes","No"), landlocked_long= ifelse(landlocked,"Yes","No"))
+
+# Add latest nominal GDP for non-FCV comparators
+nominal_gdp <- filter(Report_data, Key %in% 28107) %>%
+  subset(Period == as.numeric(max(Period,na.rm = TRUE)) -1.0) %>%
+  mutate(latestNominalGDP = Observation)
+nominal_gdp <- nominal_gdp[c('iso2','latestNominalGDP')]
+countries <- merge(countries, nominal_gdp, by="iso2", all.x=TRUE)
+
+
+text_color <- "#404040"
+.reportGenerator("Albania", "FCV")
+
 source('Report_Generator_FCVonly.R')
